bd9af45fb9a9ab2f2bda15cd8082bc5422031a17
Long Zhang <long.zhang@electrolux.com>
2024-06-19 12:18:58 +0200

    #12 improve error handling, catch errors if some of the accounts fail
    Signed-off-by: Long Zhang <long.zhang@electrolux.com>

--- plugins/infrawallet-backend/src/service/AwsClient.ts
+++ plugins/infrawallet-backend/src/service/AwsClient.ts
@@ -5,7 +5,7 @@ import {
   Granularity,
 } from '@aws-sdk/client-cost-explorer';
 import { AssumeRoleCommand, STSClient } from '@aws-sdk/client-sts';
-import { DatabaseService } from '@backstage/backend-plugin-api';
+import { LoggerService, DatabaseService } from '@backstage/backend-plugin-api';
 import { Config } from '@backstage/config';
 import { reduce } from 'lodash';
 import moment from 'moment';
@@ -14,14 +14,15 @@ import { CostQuery, Report } from './types';
 import { getCategoryMappings, getCategoryByServiceName } from './functions';
 
 export class AwsClient implements InfraWalletApi {
-  static create(config: Config, database: DatabaseService) {
-    return new AwsClient(config, database);
+  static create(config: Config, database: DatabaseService, logger: LoggerService) {
+    return new AwsClient(config, database, logger);
   }
 
   constructor(
     private readonly config: Config,
     private readonly database: DatabaseService,
-  ) { }
+    private readonly logger: LoggerService,
+  ) {}
 
   convertServiceName(serviceName: string): string {
     let convertedName = serviceName;
@@ -33,10 +34,16 @@ export class AwsClient implements InfraWalletApi {
       ['Virtual Private Cloud', 'VPC (Virtual Private Cloud)'],
       ['Relational Database Service', 'RDS (Relational Database Service)'],
       ['Simple Storage Service', 'S3 (Simple Storage Service)'],
-      ['Managed Streaming for Apache Kafka', 'MSK (Managed Streaming for Apache Kafka)'],
-      ['Elastic Container Service for Kubernetes', 'EKS (Elastic Container Service for Kubernetes)'],
+      [
+        'Managed Streaming for Apache Kafka',
+        'MSK (Managed Streaming for Apache Kafka)',
+      ],
+      [
+        'Elastic Container Service for Kubernetes',
+        'EKS (Elastic Container Service for Kubernetes)',
+      ],
       ['Simple Queue Service', 'SQS (Simple Queue Service)'],
-      ['Simple Notification Service', 'SNS (Simple Notification Service)']
+      ['Simple Notification Service', 'SNS (Simple Notification Service)'],
     ]);
 
     for (const prefix of prefixes) {
@@ -99,84 +106,89 @@ export class AwsClient implements InfraWalletApi {
         };
       }
       const promise = (async () => {
-        const client = new STSClient(stsParams);
-        const commandInput = {
-          // AssumeRoleRequest
-          RoleArn: `arn:aws:iam::${accountId}:role/${assumedRoleName}`,
-          RoleSessionName: 'AssumeRoleSession1',
-        };
-        const assumeRoleCommand = new AssumeRoleCommand(commandInput);
-        const assumeRoleResponse = await client.send(assumeRoleCommand);
-        // init aws cost explorer client
-        const awsCeClient = new CostExplorerClient({
-          region: 'us-east-1',
-          credentials: {
-            accessKeyId: assumeRoleResponse.Credentials?.AccessKeyId as string,
-            secretAccessKey: assumeRoleResponse.Credentials
-              ?.SecretAccessKey as string,
-            sessionToken: assumeRoleResponse.Credentials
-              ?.SessionToken as string,
-          },
-        });
+        try {
+          const client = new STSClient(stsParams);
+          const commandInput = {
+            // AssumeRoleRequest
+            RoleArn: `arn:aws:iam::${accountId}:role/${assumedRoleName}`,
+            RoleSessionName: 'AssumeRoleSession1',
+          };
+          const assumeRoleCommand = new AssumeRoleCommand(commandInput);
+          const assumeRoleResponse = await client.send(assumeRoleCommand);
+          // init aws cost explorer client
+          const awsCeClient = new CostExplorerClient({
+            region: 'us-east-1',
+            credentials: {
+              accessKeyId: assumeRoleResponse.Credentials
+                ?.AccessKeyId as string,
+              secretAccessKey: assumeRoleResponse.Credentials
+                ?.SecretAccessKey as string,
+              sessionToken: assumeRoleResponse.Credentials
+                ?.SessionToken as string,
+            },
+          });
 
-        // query this aws account's cost and usage using @aws-sdk/client-cost-explorer
-        const input: GetCostAndUsageCommandInput = {
-          TimePeriod: {
-            Start: moment(parseInt(query.startTime, 10)).format('YYYY-MM-DD'),
-            End: moment(parseInt(query.endTime, 10)).format('YYYY-MM-DD'),
-          },
-          Granularity: query.granularity.toUpperCase() as Granularity,
-          Filter: { Dimensions: { Key: 'RECORD_TYPE', Values: ['Usage'] } },
-          GroupBy: [{ Type: 'DIMENSION', Key: 'SERVICE' }],
-          Metrics: ['UnblendedCost'],
-        };
+          // query this aws account's cost and usage using @aws-sdk/client-cost-explorer
+          const input: GetCostAndUsageCommandInput = {
+            TimePeriod: {
+              Start: moment(parseInt(query.startTime, 10)).format('YYYY-MM-DD'),
+              End: moment(parseInt(query.endTime, 10)).format('YYYY-MM-DD'),
+            },
+            Granularity: query.granularity.toUpperCase() as Granularity,
+            Filter: { Dimensions: { Key: 'RECORD_TYPE', Values: ['Usage'] } },
+            GroupBy: [{ Type: 'DIMENSION', Key: 'SERVICE' }],
+            Metrics: ['UnblendedCost'],
+          };
 
-        const getCostCommand = new GetCostAndUsageCommand(input);
-        const costAndusageResponse = await awsCeClient.send(getCostCommand);
+          const getCostCommand = new GetCostAndUsageCommand(input);
+          const costAndusageResponse = await awsCeClient.send(getCostCommand);
 
-        const transformedData = reduce(
-          costAndusageResponse.ResultsByTime,
-          (accumulator: { [key: string]: Report }, row) => {
-            const rowTime = row.TimePeriod?.Start;
-            const period = rowTime ? rowTime.substring(0, 7) : 'unknown';
-            if (row.Groups) {
-              row.Groups.forEach(group => {
-                const groupKeys = group.Keys ? group.Keys[0] : '';
-                const keyName = `${name}_${groupKeys}`;
+          const transformedData = reduce(
+            costAndusageResponse.ResultsByTime,
+            (accumulator: { [key: string]: Report }, row) => {
+              const rowTime = row.TimePeriod?.Start;
+              const period = rowTime ? rowTime.substring(0, 7) : 'unknown';
+              if (row.Groups) {
+                row.Groups.forEach(group => {
+                  const groupKeys = group.Keys ? group.Keys[0] : '';
+                  const keyName = `${name}_${groupKeys}`;
 
-                if (!accumulator[keyName]) {
-                  accumulator[keyName] = {
-                    id: keyName,
-                    name: `AWS/${name}`,
-                    service: this.convertServiceName(groupKeys),
-                    category: getCategoryByServiceName(
-                      groupKeys,
-                      categoryMappings,
-                    ),
-                    provider: 'AWS',
-                    reports: {},
-                    ...tagKeyValues,
-                  };
-                }
+                  if (!accumulator[keyName]) {
+                    accumulator[keyName] = {
+                      id: keyName,
+                      name: `AWS/${name}`,
+                      service: this.convertServiceName(groupKeys),
+                      category: getCategoryByServiceName(
+                        groupKeys,
+                        categoryMappings,
+                      ),
+                      provider: 'AWS',
+                      reports: {},
+                      ...tagKeyValues,
+                    };
+                  }
 
-                const groupMetrics = group.Metrics;
+                  const groupMetrics = group.Metrics;
 
-                if (groupMetrics !== undefined) {
-                  accumulator[keyName].reports[period] = parseFloat(
-                    groupMetrics.UnblendedCost.Amount ?? '0.0',
-                  );
-                }
-              });
-            }
+                  if (groupMetrics !== undefined) {
+                    accumulator[keyName].reports[period] = parseFloat(
+                      groupMetrics.UnblendedCost.Amount ?? '0.0',
+                    );
+                  }
+                });
+              }
 
-            return accumulator;
-          },
-          {},
-        );
+              return accumulator;
+            },
+            {},
+          );
 
-        Object.values(transformedData).map((value: any) => {
-          results.push(value);
-        });
+          Object.values(transformedData).map((value: any) => {
+            results.push(value);
+          });
+        } catch (e) {
+          this.logger.error(e);
+        }
       })();
       promises.push(promise);
     }
