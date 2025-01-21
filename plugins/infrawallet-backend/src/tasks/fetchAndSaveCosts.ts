e67b8f10ca9a57a79a99a22b515f51a3a0c14ec9
Long Zhang <long.zhang@electrolux.com>
2025-01-21 11:44:20 +0100

    #130 prefetch cost data for integrations and save them into db
    Signed-off-by: Long Zhang <long.zhang@electrolux.com>

---
+++ plugins/infrawallet-backend/src/tasks/fetchAndSaveCosts.ts
@@ -0,0 +1,39 @@
+import { getWallet } from '../controllers/MetricSettingController';
+import { InfraWalletClient } from '../cost-clients/InfraWalletClient';
+import { CategoryMappingService } from '../service/CategoryMappingService';
+import { COST_CLIENT_MAPPINGS } from '../service/consts';
+import { RouterOptions } from '../service/types';
+
+export async function fetchAndSaveCosts(options: RouterOptions) {
+  const { logger, config, cache, database } = options;
+
+  const categoryMappingService = CategoryMappingService.getInstance();
+  await categoryMappingService.refreshCategoryMappings();
+
+  // for now, this task only fetches costs for the default wallet
+  const defaultWallet = await getWallet(database, 'default');
+  const granularities = ['daily', 'monthly'];
+
+  if (defaultWallet !== undefined) {
+    logger.debug('fetchAndSaveCosts method executed for the default wallet');
+    for (const granularity of granularities) {
+      const promises: Promise<void>[] = [];
+      const conf = config.getConfig('backend.infraWallet.integrations');
+      for (const provider of conf.keys()) {
+        if (provider in COST_CLIENT_MAPPINGS) {
+          const client: InfraWalletClient = COST_CLIENT_MAPPINGS[provider].create(config, database, cache, logger);
+
+          const saveCostReportsToDatabasePromise = (async () => {
+            try {
+              await client.saveCostReportsToDatabase(defaultWallet, granularity);
+            } catch (e) {
+              logger.error(e);
+            }
+          })();
+          promises.push(saveCostReportsToDatabasePromise);
+        }
+      }
+      await Promise.all(promises);
+    }
+  }
+}
