456f723698a045eafccee146e2f4fedffe40316c
Gang <l.g.alexander@gmail.com>
2025-05-10 17:06:55 +0200

    Upgrade backstage and dependencies (#161)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -10,7 +10,7 @@ import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
 
-backend.add(import('@backstage/plugin-app-backend/alpha'));
+backend.add(import('@backstage/plugin-app-backend'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
@@ -19,7 +19,7 @@ backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
 
 // permission plugin
-backend.add(import('@backstage/plugin-permission-backend/alpha'));
+backend.add(import('@backstage/plugin-permission-backend'));
 backend.add(import('@backstage/plugin-permission-backend-module-allow-all-policy'));
 
 // InfraWallet plugin
