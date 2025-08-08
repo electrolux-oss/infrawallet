3fddaf6b92b04eb6dcf2e4394778a329e4d1a34e
Severin Wischmann <severinwischmann@nianticlabs.com>
2025-08-08 14:11:06 +0200

    Export other relevant type for ExtensionPoint

--- plugins/infrawallet-backend/src/index.ts
+++ plugins/infrawallet-backend/src/index.ts
@@ -2,4 +2,5 @@ export { infrawalletReportFilterExtensionPoint } from './extension';
 export type { InfrawalletFilterExtension, InfrawalletReportFilterExtensionPoint } from './extension';
 export { infraWalletPlugin as default } from './plugin';
 export * from './service/router';
-export type { ReportParameters } from './service/types';
+export type { ReportParameters, Tag } from './service/types';
+
