dc71dfa630e816f80d208935d2da988b2a588abc
Severin Wischmann <severinwischmann@nianticlabs.com>
2025-08-08 08:18:28 +0200

    Linting

--- plugins/infrawallet-backend/src/index.ts
+++ plugins/infrawallet-backend/src/index.ts
@@ -2,3 +2,4 @@ export { infrawalletReportFilterExtensionPoint } from './extension';
 export type { InfrawalletFilterExtension, InfrawalletReportFilterExtensionPoint } from './extension';
 export { infraWalletPlugin as default } from './plugin';
 export * from './service/router';
+export type { ReportParameters } from './service/types';
