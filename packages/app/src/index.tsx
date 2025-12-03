a4758fe80b7d2232fb2cf938769b8146a43c54f6
Ramil Galimov <dev.rgalim@gmail.com>
2025-12-03 11:30:10 +0100

    update backstage version

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,4 +1,5 @@
 import '@backstage/cli/asset-types';
+import '@backstage/ui/css/styles.css';
 import { default as React } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
